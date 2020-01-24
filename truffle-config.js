0cfbfa148b7346477d7450e5ed0ce6d0a788feb8
auralshin <anshulspartan141@gmail.com>
2020-01-25 03:23:48 +0530

    updated

--- truffle-config.js
+++ truffle-config.js
@@ -1,13 +1,9 @@
 module.exports = {
- contracts_directory: "#path",
- contract_build_directory: "#path",
- migration_directory: "#path"
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-      websocket: true
     }
   }
 };
