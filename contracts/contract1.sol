37d5c88c89222ed6efa5462d53c6e4afc176d194
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-02-16 16:20:49 +0530

    removed old features and error in syntax

--- contracts/contract1.sol
+++ contracts/contract1.sol
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 pragma solidity ^0.6.1;
-=======
-pragma solidity 0.5.0
-<<<<<<< HEAD
+
+
+
 contract MyContract ()
 {
 string firstname;
@@ -15,8 +14,7 @@ struct people() public{
 }
 
 }
-=======
->>>>>>> 57e2ac41ab59d5c1e27e3ac277138f43456bcb0b
+
  contract MyContracts()
  { 
       string firstname;
@@ -26,4 +24,4 @@ struct people() public{
            
       }
  }
->>>>>>> d2b562f9219ef8e86358f30b7818c6cde74c0734
+
